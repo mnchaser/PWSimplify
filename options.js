993f663ec98d3ac612dff0b69fb0816fb6e82a59
Eric Kraus <eric.kraus@gmail.com>
2024-05-10 01:49:09 -0500

    v1.1 check in

--- options.js
+++ options.js
@@ -1,31 +1,302 @@
 // Saves options to chrome.storage
 const saveOptions = () => {
-    const hoverlinksValue = document.getElementById('hoverlinks').checked;
-  
-    chrome.storage.sync.set(
-      { 'hoverlinksSetting': hoverlinksValue },
-      () => {
-        // Update status to let user know options were saved.
-        const status = document.getElementById('status');
-        status.textContent = 'Options saved. You can close this tab' + hoverlinksValue;
-        setTimeout(() => {
-          status.textContent = '';
-        }, 2000);
-      }
-    );
-  };
+    try
+    {
+      //NAV
+      const showBanner = document.getElementById('showBanner').checked;
+      const hoverTimeLinks = document.getElementById('hoverTimeLinks').checked;
+      const fixSliderHover = document.getElementById('fixSliderHover').checked;
+      
+      //MODELS - Globals
+      const modelShowCFS = document.getElementById('modelShowCFS').checked;
+      const modelShowECMWF = document.getElementById('modelShowECMWF').checked;
+      const modelShowECMWFAIFS = document.getElementById('modelShowECMWFAIFS').checked;
+      const modelShowGFS = document.getElementById('modelShowGFS').checked;
+      const modelShowGraphCastGFS = document.getElementById('modelShowGraphCastGFS').checked;
+      const modelShowGDPS = document.getElementById('modelShowGDPS').checked;
+      const modelShowICON = document.getElementById('modelShowICON').checked;
+      const modelShowUkmet = document.getElementById('modelShowUkmet').checked;
+
+      //MODELS - Regional
+      const modelShowNAM = document.getElementById('modelShowNAM').checked;
+      const modelShowRAP = document.getElementById('modelShowRAP').checked;
+      const modelShowRDPS = document.getElementById('modelShowRDPS').checked;
+
+      //MODELS - CAMs
+      const modelShowHRRR = document.getElementById('modelShowHRRR').checked;
+      const modelShowNAM3km = document.getElementById('modelShowNAM3km').checked;
+      const modelShowHRWWRFARW = document.getElementById('modelShowHRWWRFARW').checked;
+      const modelShowHRWWRFNSSL = document.getElementById('modelShowHRWWRFNSSL').checked;
+      const modelShowFV3 = document.getElementById('modelShowFV3').checked;
+      const modelShowRRFSA = document.getElementById('modelShowRRFSA').checked;
+      const modelShowNSSLMPASHN = document.getElementById('modelShowNSSLMPASHN').checked;
+      const modelShowNSSLMPASHT = document.getElementById('modelShowNSSLMPASHT').checked;
+      const modelShowNSSLMPASRT = document.getElementById('modelShowNSSLMPASRT').checked;
+      const modelShowHRDPS = document.getElementById('modelShowHRDPS').checked;
+
+      //MODELS - Ensembles
+      const modelShowCMCE = document.getElementById('modelShowCMCE').checked;
+      const modelShowEPS = document.getElementById('modelShowEPS').checked;
+      const modelShowEPSOpenData = document.getElementById('modelShowEPSOpenData').checked;
+      const modelShowGEFS = document.getElementById('modelShowGEFS').checked;
+      const modelShowICONEPS = document.getElementById('modelShowICONEPS').checked;
+      const modelShowSREF = document.getElementById('modelShowSREF').checked;
+      const modelShowNWSBlend = document.getElementById('modelShowNWSBlend').checked;
+
+      //STYLE
+      const styleHighlight00Z = document.getElementById('styleHighlight00Z').checked;
+
+
+      var key = "PWSimplifySettings",
+          tempPrefs = JSON.stringify({
+              'showBanner': showBanner,
+              'hoverTimeLinks': hoverTimeLinks,
+              'fixSliderHover': fixSliderHover,
+              'modelShowCFS': modelShowCFS,
+              'modelShowECMWF': modelShowECMWF,
+              'modelShowECMWFAIFS': modelShowECMWFAIFS,
+              'modelShowGFS': modelShowGFS,
+              'modelShowGraphCastGFS': modelShowGraphCastGFS,
+              'modelShowGDPS': modelShowGDPS,
+              'modelShowICON': modelShowICON,
+              'modelShowUkmet': modelShowUkmet,
+              'modelShowNAM': modelShowNAM,
+              'modelShowRAP': modelShowRAP,
+              'modelShowRDPS': modelShowRDPS,
+              'modelShowHRRR': modelShowHRRR,
+              'modelShowNAM3km': modelShowNAM3km,
+              'modelShowHRWWRFARW': modelShowHRWWRFARW,
+              'modelShowHRWWRFNSSL': modelShowHRWWRFNSSL,
+              'modelShowFV3': modelShowFV3,
+              'modelShowRRFSA': modelShowRRFSA,
+              'modelShowNSSLMPASHN': modelShowNSSLMPASHN,
+              'modelShowNSSLMPASHT': modelShowNSSLMPASHT,
+              'modelShowNSSLMPASRT': modelShowNSSLMPASRT,
+              'modelShowHRDPS': modelShowHRDPS,
+              'modelShowCMCE': modelShowCMCE,
+              'modelShowEPS': modelShowEPS,
+              'modelShowEPSOpenData': modelShowEPSOpenData,
+              'modelShowGEFS': modelShowGEFS,
+              'modelShowICONEPS': modelShowICONEPS,
+              'modelShowSREF': modelShowSREF,
+              'modelShowNWSBlend': modelShowNWSBlend,
+              'styleHighlight00Z': styleHighlight00Z
+          });
+      var jsonSettings = {};
+      jsonSettings[key] = tempPrefs;
+
+      //alert(tempPrefs);
+      chrome.storage.sync.set(jsonSettings, function() {
+          // Update status to let user know options were saved.
+          const status = document.getElementById('status');
+          status.textContent = 'Options saved! You can close this tab.';
+          setTimeout(() => {
+            status.textContent = '';
+          }, 2000);
+        }
+      );
+    } catch(error) {}
+  };  
   
   // Restores select box and checkbox state using the preferences
   // stored in chrome.storage.
   const restoreOptions = () => {
-
-   chrome.storage.sync.get().then(
-      (items) => {
-        document.getElementById('hoverlinks').checked = items['hoverlinks'];
-        alert('restore'+items['hoverlinks']);
-      }
-    );
+    
+  try 
+  {
+    chrome.storage.sync.get().then(
+        (items) => {
+          var jsonSettingsVal = items['PWSimplifySettings'];
+          if (jsonSettingsVal){
+            //alert(jsonSettingsVal);
+            
+            return populateSettings(jsonSettingsVal); //pass the raw JSON
+          }
+          else
+          {
+            return saveDefaultSettings();
+          }
+        }
+      );
+    } catch(error) {}
   };
   
+  function saveDefaultSettings()
+  {
+    return new Promise(function(resolve, reject) {
+    try{
+      var key = "PWSimplifySettings",
+            tempPrefs = JSON.stringify({
+              'showBanner': true,
+              'hoverTimeLinks': true,
+              'fixSliderHover': false,
+              'modelShowCFS': true,
+              'modelShowECMWF': true,
+              'modelShowECMWFAIFS': true,
+              'modelShowGFS': true,
+              'modelShowGraphCastGFS': true,
+              'modelShowGDPS': true,
+              'modelShowICON': true,
+              'modelShowUkmet': true,
+              'modelShowNAM': true,
+              'modelShowRAP': true,
+              'modelShowRDPS': true,
+              'modelShowHRRR': true,
+              'modelShowNAM3km': true,
+              'modelShowHRWWRFARW': true,
+              'modelShowHRWWRFNSSL': true,
+              'modelShowFV3': true,
+              'modelShowRRFSA': true,
+              'modelShowNSSLMPASHN': true,
+              'modelShowNSSLMPASHT': true,
+              'modelShowNSSLMPASRT': true,
+              'modelShowHRDPS': true,
+              'modelShowCMCE': true,
+              'modelShowEPS': true,
+              'modelShowEPSOpenData': true,
+              'modelShowGEFS': true,
+              'modelShowICONEPS': true,
+              'modelShowSREF': true,
+              'modelShowNWSBlend': true,
+              'styleHighlight00Z': false
+            });
+        var jsonSettings = {};
+        jsonSettings[key] = tempPrefs;
+
+        //alert(tempPrefs);
+        chrome.storage.sync.set(jsonSettings, function() {
+
+            setTimeout(() => {
+              //do something
+              return populateSettings(tempPrefs);
+            }, 3000);
+          }
+        );
+      } catch(error) {reject(false);}
+    });
+  }
+
+  function populateSettings(theSettings) {
+    return new Promise(function(resolve, reject) {
+      try{
+        //passed in the raw JSON
+        if(theSettings){
+          var retrSettings = JSON.parse(theSettings);
+
+          //alert('in populate! with ' + retrSettings);
+
+          //NAV
+          var showBanner = retrSettings['showBanner'];
+          //alert('show banner' + showBanner);
+          document.getElementById('showBanner').checked = showBanner;
+
+          var hoverTimeLinks = retrSettings['hoverTimeLinks'];
+          document.getElementById('hoverTimeLinks').checked = hoverTimeLinks;
+
+          var fixSliderHover = retrSettings['fixSliderHover'];
+          document.getElementById('fixSliderHover').checked = fixSliderHover;
+
+          //MODELS - Globals
+          var modelShowCFS = retrSettings['modelShowCFS'];
+          document.getElementById('modelShowCFS').checked = modelShowCFS;
+
+          var modelShowECMWF = retrSettings['modelShowECMWF'];
+          document.getElementById('modelShowECMWF').checked = modelShowECMWF;
+
+          var modelShowECMWFAIFS = retrSettings['modelShowECMWFAIFS'];
+          document.getElementById('modelShowECMWFAIFS').checked = modelShowECMWFAIFS;
+
+          var modelShowGFS = retrSettings['modelShowGFS'];
+          document.getElementById('modelShowGFS').checked = modelShowGFS;
+
+          var modelShowGraphCastGFS = retrSettings['modelShowGraphCastGFS'];
+          document.getElementById('modelShowGraphCastGFS').checked = modelShowGraphCastGFS;
+          
+          var modelShowGDPS = retrSettings['modelShowGDPS'];
+          document.getElementById('modelShowGDPS').checked = modelShowGDPS;
+          
+          var modelShowICON = retrSettings['modelShowICON'];
+          document.getElementById('modelShowICON').checked = modelShowICON;
+          
+          var modelShowUkmet = retrSettings['modelShowUkmet'];
+          document.getElementById('modelShowUkmet').checked = modelShowUkmet;
+
+          //MODELS - Region
+          var modelShowNAM = retrSettings['modelShowNAM'];
+          document.getElementById('modelShowNAM').checked = modelShowNAM;
+
+          var modelShowRAP = retrSettings['modelShowRAP'];
+          document.getElementById('modelShowRAP').checked = modelShowRAP;
+
+          var modelShowRDPS = retrSettings['modelShowRDPS'];
+          document.getElementById('modelShowRDPS').checked = modelShowRDPS;
+
+          //MODELS - CAMs
+          var modelShowHRRR = retrSettings['modelShowHRRR'];
+          document.getElementById('modelShowHRRR').checked = modelShowHRRR;
+
+          var modelShowNAM3km = retrSettings['modelShowNAM3km'];
+          document.getElementById('modelShowNAM3km').checked = modelShowNAM3km;
+
+          var modelShowHRWWRFARW = retrSettings['modelShowHRWWRFARW'];
+          document.getElementById('modelShowHRWWRFARW').checked = modelShowHRWWRFARW;
+
+          var modelShowHRWWRFNSSL = retrSettings['modelShowHRWWRFNSSL'];
+          document.getElementById('modelShowHRWWRFNSSL').checked = modelShowHRWWRFNSSL;
+
+          var modelShowFV3 = retrSettings['modelShowFV3'];
+          document.getElementById('modelShowFV3').checked = modelShowFV3;
+
+          var modelShowRRFSA = retrSettings['modelShowRRFSA'];
+          document.getElementById('modelShowRRFSA').checked = modelShowRRFSA;
+
+          var modelShowNSSLMPASHN = retrSettings['modelShowNSSLMPASHN'];
+          document.getElementById('modelShowNSSLMPASHN').checked = modelShowNSSLMPASHN;
+
+          var modelShowNSSLMPASHT = retrSettings['modelShowNSSLMPASHT'];
+          document.getElementById('modelShowNSSLMPASHT').checked = modelShowNSSLMPASHT;
+
+          var modelShowNSSLMPASRT = retrSettings['modelShowNSSLMPASRT'];
+          document.getElementById('modelShowNSSLMPASRT').checked = modelShowNSSLMPASRT;
+
+          var modelShowHRDPS = retrSettings['modelShowHRDPS'];
+          document.getElementById('modelShowHRDPS').checked = modelShowHRDPS;
+
+          //MODELS - Ensembles
+          var modelShowCMCE = retrSettings['modelShowCMCE'];
+          document.getElementById('modelShowCMCE').checked = modelShowCMCE;
+
+          var modelShowEPS = retrSettings['modelShowEPS'];
+          document.getElementById('modelShowEPS').checked = modelShowEPS;
+
+          var modelShowEPSOpenData = retrSettings['modelShowEPSOpenData'];
+          document.getElementById('modelShowEPSOpenData').checked = modelShowEPSOpenData;
+
+          var modelShowGEFS = retrSettings['modelShowGEFS'];
+          document.getElementById('modelShowGEFS').checked = modelShowGEFS;
+
+          var modelShowICONEPS = retrSettings['modelShowICONEPS'];
+          document.getElementById('modelShowICONEPS').checked = modelShowICONEPS;
+
+          var modelShowSREF = retrSettings['modelShowSREF'];
+          document.getElementById('modelShowSREF').checked = modelShowSREF;
+
+          var modelShowNWSBlend = retrSettings['modelShowNWSBlend'];
+          document.getElementById('modelShowNWSBlend').checked = modelShowNWSBlend;
+
+          //STYLE
+          var styleHighlight00Z = retrSettings['styleHighlight00Z'];
+          document.getElementById('styleHighlight00Z').checked = styleHighlight00Z;
+
+        }
+
+        document.getElementById('body').className = "loaded";
+        resolve(theSettings);
+
+      } catch(error) { reject(error); }
+    });
+  }
+
+
   document.addEventListener('DOMContentLoaded', restoreOptions);
   document.getElementById('save').addEventListener('click', saveOptions);
--- pwscript.js
+++ pwscript.js
@@ -1,24 +1,183 @@
-//alert('hello');
-/*if(window.location)
+
+var savedSettings = null;
+
+try
 {
-    const urlParams = new window.location.href
-    urlParams.set('fh', 'loop');
-//document.location.search = urlParams;
-    alert(window.location.href);
-}*/
+    //chrome.storage.sync.clear();
 
-/*if ('URLSearchParams' in window) {
-    var searchParams = new URLSearchParams(window.location.search)
-    searchParams.set("fh", "loop");
-    window.location.search = searchParams.toString();
-    //var newRelativePathQuery = window.location.pathname + '?' + searchParams.toString();
-    //history.pushState(null, '', newRelativePathQuery);
-}*/
+    chrome.storage.sync.get().then(
+        (items) => {
+            var jsonSettingsVal = items['PWSimplifySettings'];
+            if(jsonSettingsVal){
+                
+                savedSettings = JSON.parse(jsonSettingsVal);
 
-try{
-    const sliderHandleObj = document.getElementsByClassName('ui-slider-handle');
-    sliderHandleObj[0].addEventListener("mouseout", (event) => {
-        sliderHandleObj[0].blur();
-    });
-}
-catch(error) {}
+                //if settings are available
+                if(savedSettings) {
+                    
+                    //NAV
+                    var showBanner = savedSettings['showBanner']; //#banner_container {display: none !important; }
+                    if(showBanner != null && showBanner == false) { document.getElementById('banner_container').style.display = 'none'; }
+
+                    //alert(jsonSettingsVal);
+
+                    var hoverTimeLinks = savedSettings['hoverTimeLinks']; //#hoverlinks {display: none !important; }
+                    if(hoverTimeLinks != null && hoverTimeLinks == false) 
+                    { 
+                        var hoverlnks = document.getElementById('hoverlinks');
+                        if(hoverlnks)
+                        {
+                            hoverlnks.style.display = 'none'; 
+                        }
+                    }
+
+                    var fixSliderHover = savedSettings['fixSliderHover'];
+                    if(fixSliderHover) 
+                    {
+                        const sliderHandleObj = document.getElementsByClassName('ui-slider-handle');
+                        if(sliderHandleObj)
+                        {
+                            var sliderBtn = sliderHandleObj[0];
+                            if(sliderBtn)
+                            {
+                                sliderBtn.addEventListener("mouseout", (event) => {
+                                    sliderHandleObj[0].blur();
+                                });
+                            }
+                        }
+
+                    }
+
+
+                    //MODELS - Globals
+                    var modelShowCFS = savedSettings['modelShowCFS'];
+                    if(modelShowCFS != null && modelShowCFS == false) { document.querySelector('[data-model="cfs"]').style.display = 'none'; }
+
+                    var modelShowECMWF = savedSettings['modelShowECMWF'];
+                    if(modelShowECMWF != null && modelShowECMWF == false) { document.querySelector('[data-model="ecmwf_full"]').style.display = 'none'; }
+
+                    var modelShowECMWFAIFS = savedSettings['modelShowECMWFAIFS'];
+                    if(modelShowECMWFAIFS != null && modelShowECMWFAIFS == false) { document.querySelector('[data-model="ecmwf_aifs"]').style.display = 'none'; }
+
+                    var modelShowGFS = savedSettings['modelShowGFS'];
+                    if(modelShowGFS != null && modelShowGFS == false) { document.querySelector('[data-model="gfs"]').style.display = 'none'; }
+
+                    var modelShowGraphCastGFS = savedSettings['modelShowGraphCastGFS'];
+                    if(modelShowGraphCastGFS != null && modelShowGraphCastGFS == false) { document.querySelector('[data-model="graphcast_gfs"]').style.display = 'none'; }
+
+                    var modelShowGDPS = savedSettings['modelShowGDPS'];
+                    if(modelShowGDPS != null && modelShowGDPS == false) { document.querySelector('[data-model="gdps"]').style.display = 'none'; }
+
+                    var modelShowICON = savedSettings['modelShowICON'];
+                    if(modelShowICON != null && modelShowICON == false) { document.querySelector('[data-model="icon"]').style.display = 'none'; }
+
+                    var modelShowUkmet = savedSettings['modelShowUkmet'];
+                    if(modelShowUkmet != null && modelShowUkmet == false) { document.querySelector('[data-model="ukmet"]').style.display = 'none'; }
+
+                    //MODELS - Regional
+                    var modelShowNAM = savedSettings['modelShowNAM'];
+                    if(modelShowNAM != null && modelShowNAM == false) { document.querySelector('[data-model="nam"]').style.display = 'none'; }
+
+                    var modelShowRAP = savedSettings['modelShowRAP'];
+                    if(modelShowRAP != null && modelShowRAP == false) { document.querySelector('[data-model="rap"]').style.display = 'none'; }
+
+                    var modelShowRDPS = savedSettings['modelShowRDPS'];
+                    if(modelShowRDPS != null && modelShowRDPS == false) { document.querySelector('[data-model="rdps"]').style.display = 'none'; }
+
+                    //MODELS - CAMs
+                    var modelShowHRRR = savedSettings['modelShowHRRR'];
+                    if(modelShowHRRR != null && modelShowHRRR == false) { document.querySelector('[data-model="hrrr"]').style.display = 'none'; }
+
+                    var modelShowNAM3km = savedSettings['modelShowNAM3km'];
+                    if(modelShowNAM3km != null && modelShowNAM3km == false) { document.querySelector('[data-model="nam4km"]').style.display = 'none'; }
+
+                    var modelShowHRWWRFARW = savedSettings['modelShowHRWWRFARW'];
+                    if(modelShowHRWWRFARW != null && modelShowHRWWRFARW == false) { document.querySelector('[data-model="hrwarw"]').style.display = 'none'; }
+
+                    var modelShowHRWWRFNSSL = savedSettings['modelShowHRWWRFNSSL'];
+                    if(modelShowHRWWRFNSSL != null && modelShowHRWWRFNSSL == false) { document.querySelector('[data-model="hrwnssl"]').style.display = 'none'; }
+
+                    var modelShowFV3 = savedSettings['modelShowFV3'];
+                    if(modelShowFV3 != null && modelShowFV3 == false) { document.querySelector('[data-model="hrwfv3"]').style.display = 'none'; }
+
+                    var modelShowRRFSA = savedSettings['modelShowRRFSA'];
+                    if(modelShowRRFSA != null && modelShowRRFSA == false) { document.querySelector('[data-model="rrfs_a"]').style.display = 'none'; }
+
+                    var modelShowNSSLMPASHN = savedSettings['modelShowNSSLMPASHN'];
+                    if(modelShowNSSLMPASHN != null && modelShowNSSLMPASHN == false) { document.querySelector('[data-model="mpas_nssl_hn"]').style.display = 'none'; }
+
+                    var modelShowNSSLMPASHT = savedSettings['modelShowNSSLMPASHT'];
+                    if(modelShowNSSLMPASHT != null && modelShowNSSLMPASHT == false) { document.querySelector('[data-model="mpas_nssl_ht"]').style.display = 'none'; }
+
+                    var modelShowNSSLMPASRT = savedSettings['modelShowNSSLMPASRT'];
+                    if(modelShowNSSLMPASRT != null && modelShowNSSLMPASRT == false) { document.querySelector('[data-model="mpas_nssl_rt"]').style.display = 'none'; }
+
+                    var modelShowHRDPS = savedSettings['modelShowHRDPS'];
+                    if(modelShowHRDPS != null && modelShowHRDPS == false) { document.querySelector('[data-model="hrdps"]').style.display = 'none'; }
+
+                    //MODELS - Ensembles
+                    var modelShowCMCE = savedSettings['modelShowCMCE'];
+                    if(modelShowCMCE != null && modelShowCMCE == false) { document.querySelector('[data-model="cmceens"]').style.display = 'none'; }
+
+                    var modelShowEPS = savedSettings['modelShowEPS'];
+                    if(modelShowEPS != null && modelShowEPS == false) { document.querySelector('[data-model="epsens"]').style.display = 'none'; }
+
+                    var modelShowEPSOpenData = savedSettings['modelShowEPSOpenData'];
+                    if(modelShowEPSOpenData != null && modelShowEPSOpenData == false) { document.querySelector('[data-model="epsens_opendata"]').style.display = 'none'; }
+
+                    var modelShowGEFS = savedSettings['modelShowGEFS'];
+                    if(modelShowGEFS != null && modelShowGEFS == false) { document.querySelector('[data-model="gefsens"]').style.display = 'none'; }
+
+                    var modelShowICONEPS = savedSettings['modelShowICONEPS'];
+                    if(modelShowICONEPS != null && modelShowICONEPS == false) { document.querySelector('[data-model="iconens"]').style.display = 'none'; }
+
+                    var modelShowSREF = savedSettings['modelShowSREF'];
+                    if(modelShowSREF != null && modelShowSREF == false) { document.querySelector('[data-model="srefens"]').style.display = 'none'; }
+
+                    var modelShowNWSBlend = savedSettings['modelShowNWSBlend'];
+                    if(modelShowNWSBlend != null && modelShowNWSBlend == false) { document.querySelector('[data-model="nbm"]').style.display = 'none'; }
+
+                    //COMPARE
+                    /*var compSel = document.getElementById('model_comparison_selector');
+                    if(compSel)
+                    {
+                        const compareDiv = document.createElement("div");
+                        const compareCheckboxLabel = document.createElement("label");
+                        const newLabelContent = document.createTextNode("Rotate Compare");
+                        const autoCompareChkBx = document.createElement("input");
+                        autoCompareChkBx.type = "checkbox";
+
+                        autoCompareChkBx.addEventListener('change', (event) => {
+                            if (event.currentTarget.checked) {
+                              
+                            } else {
+                              
+                            }
+                          })
+
+                        compareCheckboxLabel.appendChild(autoCompareChkBx);
+                        compareCheckboxLabel.appendChild(newLabelContent);
+                        compareDiv.appendChild(compareCheckboxLabel);
+
+                        compSel.parentNode.insertBefore(compareDiv, compSel.nextSibling);
+                    }*/
+
+                    //Highlight 00Z
+                    var styleHighlight00Z = savedSettings['styleHighlight00Z'];
+                    if(styleHighlight00Z)
+                    {
+                        var gridBoxList = document.querySelectorAll("div.button_cell[title~='00z']:not(.variable_selected)");
+                        if(gridBoxList)
+                        {
+                            gridBoxList.forEach((gridObj) => {  
+                                gridObj.style.backgroundColor = "#f64605";
+                            });
+                        }
+                    }
+                }
+            }
+        }
+      );
+
+    }
+catch(error) {alert('Oh Hail no! Sorry I ran into an issue: ' + error);}
